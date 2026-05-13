f0a4fc2f8b0990c16320921d7614049c370ba802
EXPlasmite <ty.bingham1@outlook.com>
2026-05-13 01:03:11 +0100

    Add enemy spawner with 4 spawn points

---
+++ Assets/Scripts/Enemies/Enemy Spawner.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform[] spawnPoints;
+    public float spawnInterval = 5f;
+    public int maxEnemies = 5;
+
+    private float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnInterval && 
+            GameObject.FindGameObjectsWithTag("Enemy").Length < maxEnemies)
+        {
+            SpawnEnemy();
+            timer = 0f;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        int index = Random.Range(0, spawnPoints.Length);
+        GameObject enemy = Instantiate(enemyPrefab, 
+            spawnPoints[index].position, Quaternion.identity);
+    }
+}
