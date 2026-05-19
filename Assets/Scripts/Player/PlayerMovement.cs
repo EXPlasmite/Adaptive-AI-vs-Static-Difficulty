b17da80a3a0d7e3c368b37e0e5583c71eb0217d5
EXPlasmite <ty.bingham1@outlook.com>
2026-05-19 06:53:27 +0100

    Add player sprite with movement animations and correct facing direction

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,23 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-
     private Rigidbody2D rb;
     private Vector2 movement;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.flipX = true;
     }
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        // Flip sprite based on horizontal direction
+        if (movement.x > 0)
+            spriteRenderer.flipX = false;
+        else if (movement.x < 0)
+            spriteRenderer.flipX = true;
+
+        // Trigger animations
+        if (animator != null)
+        {
+            if (movement.magnitude > 0)
+            {
+                animator.Play("walk");
+                animator.SetFloat("movementY", movement.y > 0 ? 1f : -1f);
+            }
+            else
+            {
+                animator.Play("idle");
+                animator.SetFloat("movementY", -1f);
+            }
+        }
     }
 
     void FixedUpdate()
